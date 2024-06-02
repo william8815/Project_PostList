9404bd592ca6faa0be287e71456fd254185e1be6
william8815 <williamhsu88157976@gmail.com>
2024-06-02 23:18:49 +0800

    feat isAuth middleware in image file router

--- routes/upload.js
+++ routes/upload.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 const handleErrorAsync = require("../services/handleErrorAsync");
 const upload = require("../services/image");
+const { isAuth } = require("../services/auth");
 
 const uploadController = require("../controllers/upload");
 // 下方的 req.files 是由 middleware: multer 的 any() 語法產生出
-router.post("/file", upload, handleErrorAsync(uploadController.uploadImage));
+router.post(
+  "/file",
+  isAuth,
+  upload,
+  handleErrorAsync(uploadController.uploadImage)
+);
 module.exports = router;
